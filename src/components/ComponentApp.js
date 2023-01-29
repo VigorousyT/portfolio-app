b438dd1c0b196c54c77a35afcd75a177be99718d
VigorousyT <harshyadav082000@gmail.com>
2023-01-29 19:03:59 +0530

    portfolio app

---
+++ src/components/ComponentApp.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Navbar from './Navbar/Navbar';
+import Home from './Home/Home';
+import About from './About/About';
+import { Outlet } from 'react-router';
+import Skills from './Skills/Skills';
+import Achievement from './Achievement/Achievement';
+import Contact from './Contact/Contact';
+import Project from './Project/Project';
+import Footer from './Footer/Footer';
+import ScrollUp from './ScrollUp/ScrollUp';
+
+const ComponentApp = () => {
+    return (
+        <>
+            <Navbar></Navbar>
+            <main className='main'>
+                <Home />
+                <About />
+                <Skills />
+                <Project />
+                <Achievement />
+                <Contact />
+                <Outlet />
+                <ScrollUp />
+                <Footer />
+            </main>
+
+        </>
+    )
+}
+
+export default ComponentApp;

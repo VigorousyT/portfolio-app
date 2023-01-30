1dcaa7cf43b3eb224fb68df37cb243e1391cf393
VigorousyT <harshyadav082000@gmail.com>
2023-01-30 15:22:08 +0530

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
   window.addEventListener("scroll", function () {
     const header = this.document.querySelector(".header");
     //when the scroll is higher than 560vh height, add the scroll-header class to a tag with the scroll-top class
-    if (this.scrollY >= 80) header.classList.add("scroll-header");
+    if (this.scrollY >= 280) header.classList.add("scroll-header");
     else header.classList.remove("scroll-header");
   });
 
